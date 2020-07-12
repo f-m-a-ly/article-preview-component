95a26872869a7bd3ce1d5fda3405c9870376b9c0
Farah Aly <57357753+f-m-a-ly@users.noreply.github.com>
2020-07-12 19:54:14 +0100

    project reupload - error with git

---
+++ app.js
@@ -0,0 +1,9 @@
+// $(document).ready(function(){
+//     $('[data-toggle="tooltip"]').tooltip();  
+//   });
+
+  $(document).ready(function(){
+    $("#share-icon").click(function(){
+      $("[data-toggle='tooltip']").tooltip('toggle');
+    });
+  });
